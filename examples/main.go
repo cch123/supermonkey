0be7a12bfe98934d7c3d37f7f13902d3dc37ad59
Xargin <cao1988228@163.com>
2020-09-03 20:02:50 +0800

    add example

---
+++ examples/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	sm "github.com/cch123/supermonkey"
+)
+
+func main() {
+	fmt.Println("original function output:")
+	heyHey()
+	fmt.Println()
+
+	sm.Patch("main", "", "heyHey", func() {
+		fmt.Println("please be polite")
+	})
+	fmt.Println("after patch, function output:")
+	heyHey()
+	fmt.Println()
+
+	sm.UnpatchAll()
+	fmt.Println("unpatch all, then output:")
+	heyHey()
+}
+
+//go:noinline
+func heyHey() {
+	fmt.Println("fuck")
+}
